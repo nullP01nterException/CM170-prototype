3b3ff06fdeb32c59a451d7afff82aa6fbdccd79a
Kevin Wu <Kevin Wu>
2017-10-30 12:38:40 -0700

    pushing Kevin stuff with Audio Spectrum Data

---
+++ Assets/Scripts/createSpawners.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createSpawners : MonoBehaviour {
+
+	public GameObject spawner1;
+	public GameObject spawner2;
+	public GameObject spawner3;
+
+	public List<GameObject> spawnerList;
+	public static createSpawners current;
+	// Use this for initialization
+	void Start () {
+		spawnerList = new List<GameObject> ();
+		GameObject obj1 = Instantiate (spawner1);
+		GameObject obj2 = Instantiate (spawner2);
+		GameObject obj3 = Instantiate (spawner3);
+
+		spawnerList.Add (obj1);
+		spawnerList.Add (obj2);
+		spawnerList.Add (obj3);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/masterFreqGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class masterFreqGenerator : MonoBehaviour {
+
+
+	//grab all static params
+	//createSpawners.current.spawnerList
+	//Audiopeer._samples
+	//
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
